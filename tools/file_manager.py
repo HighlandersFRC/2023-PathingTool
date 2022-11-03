4eaf0370e44f4c75bb2c298f3adc80628963fa06
Paul-Mick <paulmickrobotics@gmail.com>
2022-11-03 11:21:26 -0600

    added ssh code, needs testing

--- tools/file_manager.py
+++ tools/file_manager.py
@@ -7,6 +7,48 @@ import scp
 from data_assets.point import Point
 from SplineGeneration import generateSplines
 
+def get_connection(addr: str):
+    try:
+        ssh_cli = paramiko.SSHClient()
+        ssh_cli.connect(addr, username = "lvuser", password = "", timeout = 1)
+        scp_cli = scp.SCPClient(ssh_cli.get_transport())
+        print("Got connection successfully")
+        return scp_cli, ssh_cli
+    except TimeoutError:
+        print("Connection timed out")
+        return
+    except:
+        print("Error connecting")
+        return
+
+def upload_all(addr: str):
+    saves = glob.glob("saves/*.json*")
+    scp_cli = get_connection(addr)[0]
+    if scp_cli == None:
+        return
+    for save in saves:
+        scp_cli.put(save, remote_path = "/home/lvuser/deploy")
+    print("Uploaded all save files")
+
+def upload(addr: str, file_path: str):
+    scp_cli = get_connection(addr)[0]
+    if scp_cli == None:
+        return
+    scp_cli.put(file_path, remote_path = "/home/lvuser/deploy")
+    print("Uploaded save successfully")
+
+def download_all(addr: str):
+    conns = get_connection(addr)
+    scp_cli = conns[0]
+    ssh_cli = conns[1]
+    sftp = ssh_cli.open_sftp()
+    remote_saves = sftp.listdir("/home/lvuser/deploy")
+    for rs in remote_saves:
+        if rs.endswith(".json"):
+            scp_cli.get(remote_path = f"/home/lvuser/deploy/{rs}", local_path = "saves/")
+    print("Downloaded all saves successfully")
+
+
 def save_path(key_points: list, sample_rate: float, folder_path: str, file_name: str):
     data = {}
     data["meta_data"] = {
