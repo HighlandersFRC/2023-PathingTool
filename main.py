be35985919915ebeb4eb9c94a4a7851661538acf
Paul-Mick <paulmickrobotics@gmail.com>
2022-12-10 13:33:25 -0700

    version 1.1.0

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ class PathApp(App):
     def build(self):
         #schedule execute callback
         Clock.schedule_interval(self.execute, 1.0  / 60.0)
-        self.title = "2023 Path Tool - Highlanders FRC #4499"
+        self.title = "2023 Path Tool - Highlanders FRC #4499 - Version 1.1.0"
         self.icon = "images/4499Icon.ico"
         self.path_tool = PathTool()
         return self.path_tool
