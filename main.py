347266f882578e7b0b5fc97b9c5bd3616323a3e6
Paul-Mick <paulmickrobotics@gmail.com>
2022-12-10 11:56:37 -0700

    changed window icon

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ class PathApp(App):
         #schedule execute callback
         Clock.schedule_interval(self.execute, 1.0  / 60.0)
         self.title = "2023 Path Tool - Highlanders FRC #4499"
+        self.icon = "images/4499Icon.ico"
         self.path_tool = PathTool()
         return self.path_tool
 
